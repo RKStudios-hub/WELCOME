aaeb8e0d3bc3f784662a2d3b8c74d9a95de8c732
🌈RK_Studios🚀 <hrupeshkumarh@gmail.com>
2024-06-18 19:27:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        const centerDiv = document.querySelector('.center');
+        centerDiv.style.display = 'none';
+    }, 4000); // 4000 milliseconds = 4 seconds
+});
