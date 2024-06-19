44589b1419d60f5e8b0f234d6561ea94eaf7304c
🌈RK_Studios🚀 <hrupeshkumarh@gmail.com>
2024-06-19 08:35:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         const centerDiv = document.querySelector('.center');
         centerDiv.style.display = 'none';
-    }, 4000); // 4000 milliseconds = 4 seconds
+    }, 6000); // 4000 milliseconds = 4 seconds
 });
